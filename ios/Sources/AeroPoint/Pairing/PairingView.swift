ffb6b37fdcfc063c0cdb30415e89189d96f9d311
dannywandesign <yunhuw@gmail.com>
2026-05-23 15:58:07 -0400

    style: align scanner corner guidelines to rounded scanning box radius

--- ios/Sources/AeroPoint/Pairing/PairingView.swift
+++ ios/Sources/AeroPoint/Pairing/PairingView.swift
@@ -372,12 +372,23 @@ struct ScannerTargetFrameView: View {
 }
 
 struct ScannerCornerShape: Shape {
+    let radius: CGFloat = 24
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        let length: CGFloat = 24
+        let length: CGFloat = 24 + radius
         
         path.move(to: CGPoint(x: rect.minX, y: rect.minY + length))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + radius))
+        
+        path.addArc(
+            center: CGPoint(x: rect.minX + radius, y: rect.minY + radius),
+            radius: radius,
+            startAngle: .degrees(180),
+            endAngle: .degrees(270),
+            clockwise: false
+        )
+        
         path.addLine(to: CGPoint(x: rect.minX + length, y: rect.minY))
         
         return path
